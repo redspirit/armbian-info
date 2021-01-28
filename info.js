4d02810b6c6eca45cd8e44e2c055fa7ef6075a96
Alexey <redspirit@live.ru>
2021-01-28 17:24:03 +0300

    exec

--- info.js
+++ info.js
@@ -1,14 +1,13 @@
+const { exec } = require("child_process");
+
 module.exports = () => {
 
 
+    exec("ifconfig tun0", (error, stdout, stderr) => {
 
+        console.log(stdout);
 
-
-
-
-
-
-
+    });
 
 
 
